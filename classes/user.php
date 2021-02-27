78871acfc8745b0376911fc1e53f334a3047ff67
Erjon <erjonlusha@gmail.com>
2021-02-27 13:32:30 +0100

    Corretto

--- classes/user.php
+++ classes/user.php
@@ -1,5 +1,5 @@
 <?php
-include_once __DIR__ . '.commenti.php';
+include_once __DIR__ . '/commenti.php';
 /** Commenti class
  * @author Erion
  * @copyright 2021 Erion
@@ -10,4 +10,36 @@ class User extends Commenti{
     public $name;
     public $surname;
     public $email;
+    
+    public function __construct(string  $idArticolo, string  $txt_comment, string  $voto, string  $risp, string $idUser, string $name, string $surname, string $email) {
+        parent::__construct($idArticolo, $txt_comment, $voto, $risp);
+        $this->idUser = $idUser;
+        $this->name = $name;
+        $this->surname = $surname;
+        $this->email = $email;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    public function getTxt_comment()
+    {
+        return $this->txt_comment;
+    }
+
+    public function getRisp()
+    {
+        return $this->risp;
+    }
 }
